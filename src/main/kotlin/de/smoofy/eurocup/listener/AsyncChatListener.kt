63411165bec88ed2d55489a57e0bbb6e46e02da4
Smoofy19 <nxcoop19@gmail.com>
2024-06-18 12:06:41 +0200

    Implemented fan chat

--- src/main/kotlin/de/smoofy/eurocup/listener/AsyncChatListener.kt
+++ src/main/kotlin/de/smoofy/eurocup/listener/AsyncChatListener.kt
@@ -1,11 +1,14 @@
 package de.smoofy.eurocup.listener
 
 import de.smoofy.eurocup.EuroCup
+import de.smoofy.eurocup.fan.Country
 import de.smoofy.eurocup.player.Rank
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextReplacementConfig
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
@@ -31,15 +34,26 @@ class AsyncChatListener : Listener {
             player.bukkitPlayer().sendMessage(Component.text("You have been banned from the chat!", NamedTextColor.RED))
             return
         }
-        event.renderer { _, sourceDisplayName, message, _ ->
-            val messageContent = PlainTextComponentSerializer.plainText().serialize(message)
-            if (player.hasPriority(Rank.ADMIN.priority)) {
-                Component.text().append(sourceDisplayName.color(player.rank.color), Component.text(": ", NamedTextColor.GRAY),
-                    EuroCup.miniMessage.deserialize(messageContent)).build()
-            } else {
-                Component.text().append(sourceDisplayName.color(player.rank.color), Component.text(": ", NamedTextColor.GRAY),
-                    message).build()
+        var prefix: Component = Component.empty()
+        var suffix: Component = Component.empty()
+        var message = event.message()
+        if (player.hasPriority(Rank.ADMIN.priority)) {
+            message = EuroCup.miniMessage.deserialize(EuroCup.miniMessage.serialize(message).replace("\\", ""))
+        }
+        if (PlainTextComponentSerializer.plainText().serialize(message).startsWith("@fans ") && player.country != Country.NONE) {
+            prefix = EuroCup.miniMessage.deserialize("<gray>[${EuroCup.GRADIENT}FanChat</gradient><gray>] ")
+            message = message.replaceText(TextReplacementConfig.builder().match("@fans ").once().replacement("").build())
+
+            event.viewers().removeIf { audience -> audience is Player &&
+                    EuroCup.INSTANCE.playerManager.euroCupPlayer(audience).country != player.country &&
+                    !EuroCup.INSTANCE.playerManager.euroCupPlayer(audience).hasPriority(Rank.ADMIN.priority)
             }
+        }
+        if (player.country != Country.NONE) {
+            suffix = EuroCup.miniMessage.deserialize(" <gray>[<red>${player.country.countryCode}<gray>]")
+        }
+        event.renderer { _, _, _, _ ->
+            Component.text().append(prefix, player.displayName(), suffix, Component.text(": ", NamedTextColor.GRAY), message).build()
         }
     }
 }
