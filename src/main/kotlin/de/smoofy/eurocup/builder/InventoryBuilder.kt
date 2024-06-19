ba7c99731888c707316368f791fef2a9655fb00c
Smoofy19 <nxcoop19@gmail.com>
2024-06-19 20:50:28 +0200

    Initial commit

---
+++ src/main/kotlin/de/smoofy/eurocup/builder/InventoryBuilder.kt
@@ -0,0 +1,54 @@
+package de.smoofy.eurocup.builder
+
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
+
+
+/*
+ * Copyright ©
+ * @author - Smoofy
+ * @GitHub - https://github.com/Smoofy19
+ * @Twitter - https://twitter.com/Smoofy19
+ * Created - 19.06.2024 - 18:06
+ */
+
+/**
+
+ */
+class InventoryBuilder(holder: InventoryHolder, size: Int, title: Component) {
+
+    val inventory: Inventory
+
+    init {
+        this.inventory = Bukkit.createInventory(holder, size, title)
+    }
+
+    fun fill(item: ItemBuilder): InventoryBuilder {
+        for (i in 0 until inventory.size) {
+            if (inventory.getItem(i) == null) {
+                inventory.setItem(i, item.build())
+            }
+        }
+        return this
+    }
+
+    fun set(builder: ItemBuilder, vararg slots: Int): InventoryBuilder {
+        for (slot in slots) {
+            inventory.setItem(slot, builder.build())
+        }
+        return this
+    }
+
+    fun set(builder: SkullBuilder, vararg slots: Int): InventoryBuilder {
+        for (slot in slots) {
+            inventory.setItem(slot, builder.build())
+        }
+        return this
+    }
+
+    fun build(): Inventory {
+        return this.inventory
+    }
+}
