017ba9d65daf3dddca1086299f2faad383be2430
Smoofy19 <nxcoop19@gmail.com>
2024-06-19 20:53:22 +0200

    Initial commit

---
+++ src/main/kotlin/de/smoofy/eurocup/utils/Skulls.kt
@@ -0,0 +1,18 @@
+package de.smoofy.eurocup.utils
+
+
+/*
+ * Copyright ©
+ * @author - Smoofy
+ * @GitHub - https://github.com/Smoofy19
+ * @Twitter - https://twitter.com/Smoofy19
+ * Created - 19.06.2024 - 17:48
+ */
+
+/**
+
+ */
+enum class Skulls(val texture: String) {
+
+    FOOTBALL("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTlmNTEyZWQ2YTJhYzdjZDU1MDdkZDkzNmI2YWMyZTcwOGM0MDEwOWU3MjhmOWEyM2I1MDE1ZmM2NzkxNSJ9fX0=")
+}
