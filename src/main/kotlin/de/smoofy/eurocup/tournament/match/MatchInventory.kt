c65a354aefe9cb7711a831769036e87e115acf14
Smoofy19 <nxcoop19@gmail.com>
2024-06-19 22:05:05 +0200

    Initial commit

---
+++ src/main/kotlin/de/smoofy/eurocup/tournament/match/MatchInventory.kt
@@ -0,0 +1,18 @@
+package de.smoofy.eurocup.tournament.match
+
+
+/*
+ * Copyright ©
+ * @author - Smoofy
+ * @GitHub - https://github.com/Smoofy19
+ * @Twitter - https://twitter.com/Smoofy19
+ * Created - 19.06.2024 - 22:04
+ */
+
+/**
+
+ */
+class MatchInventory {
+
+    // TODO
+}
---
+++ src/main/kotlin/de/smoofy/eurocup/tournament/match/MatchesListener.kt
@@ -0,0 +1,71 @@
+package de.smoofy.eurocup.tournament.match
+
+import de.smoofy.eurocup.EuroCup
+import de.smoofy.eurocup.utils.Keys
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataType
+
+
+/*
+ * Copyright ©
+ * @author - Smoofy
+ * @GitHub - https://github.com/Smoofy19
+ * @Twitter - https://twitter.com/Smoofy19
+ * Created - 19.06.2024 - 21:45
+ */
+
+/**
+
+ */
+class MatchesListener : Listener {
+
+    @EventHandler
+    fun onClick(event: InventoryClickEvent) {
+        if (event.whoClicked !is Player) {
+            return
+        }
+        if (event.clickedInventory == null) {
+            return
+        }
+        if (event.clickedInventory!!.holder == null) {
+            return
+        }
+        if (event.clickedInventory!!.holder !is MatchesInventory.Holder) {
+            return
+        }
+        event.isCancelled = true
+        val player = event.whoClicked as Player
+
+        val item = event.currentItem ?: return
+        if (this.page(item) != -1) {
+            player.openInventory(MatchesInventory(this.page(item)).groupInventory())
+            return
+        }
+
+        val persistentDataContainer = item.itemMeta.persistentDataContainer
+        if (persistentDataContainer.has(Keys.MATCH.key)) {
+            val match = EuroCup.tournamentAPI.match(persistentDataContainer.get(Keys.MATCH.key, PersistentDataType.INTEGER)!!) ?: return
+            player.sendMessage("Open match inventory ${match.matchId}")
+            // TODO: open match inventory
+            return
+        }
+    }
+
+    private fun page(item: ItemStack): Int {
+        val persistentDataContainer = item.itemMeta.persistentDataContainer
+
+        if (persistentDataContainer.has(Keys.PREVIOUS_PAGE.key, PersistentDataType.INTEGER)) {
+            return persistentDataContainer.get(Keys.PREVIOUS_PAGE.key, PersistentDataType.INTEGER)!!
+        }
+
+        if (persistentDataContainer.has(Keys.NEXT_PAGE.key, PersistentDataType.INTEGER)) {
+            return persistentDataContainer.get(Keys.NEXT_PAGE.key, PersistentDataType.INTEGER)!!
+        }
+
+        return -1
+    }
+}
